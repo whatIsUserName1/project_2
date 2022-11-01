edf9714ae944c1093d1427f7324489eddacee0d7
wangchunlong <2357216471@qq.com>
2022-11-01 16:54:38 +0800

    路由问题解决

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const routes = [
       },
       {
         path: 'user-avatar',
-        component: () => import('@/views/user/userInfo.vue')
+        component: () => import('@/views/user/userAvatar.vue')
       },
       {
         path: 'user-pwd',
